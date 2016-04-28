722e6464025263df2bb2821f51e5ffcee4f77a63
Jeff <jeffrey.borowitz@giantoak.com>
2016-04-28 12:11:50 +0000

    Use Svebor's image info, run only phone level with distribution features

--- notebooks/go_notebooks/First Classifier.py
+++ notebooks/go_notebooks/First Classifier.py
@@ -23,15 +23,17 @@ from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 import ipdb
 
-df = pd.read_csv('../../data/merged/data_to_use_by_ad_v2.csv')
+#df = pd.read_csv('../../data/merged/data_to_use_by_ad_v2.csv')
+df = pd.read_csv('../../data/merged/data_to_use_by_ad_v3_with_exp_imgs.csv')
 
 steve = pd.read_csv('../../data/merged/phones_by_month.csv')
 steve_phone=steve[['phone','n_ads','n_distinct_locations','location_tree_length','n_outcall','n_incall','n_incall_and_outcall','n_cooccurring_phones']].drop_duplicates()
 df = df.merge(steve_phone, how='left')
+df = df.loc[df[['dd_id','phone']].drop_duplicates().index]
 
+df['has_images'] = df['images_count'].notnull()
 # In[2]:
 
-print(df.columns)
 
 
 # ## Induced features, relabeling, etc.
@@ -81,17 +83,24 @@ splits = [x for x in splitter]
 # ### Slice DF
 
 # In[8]:
-continuous_vars = ['age', 'price', 'duration_in_mins', 'price_per_min', 'n_ads', 'n_distinct_locations', 'location_tree_length', 'n_outcall', 'n_incall', 'n_incall_and_outcall', 'n_cooccurring_phones', 'price_per_min' ]
-continuous_means = df.groupby('phone')[continuous_vars].mean()
-continuous_means = continuous_means.rename(columns = {k+'_mean':k for k in continuous_means.columns})
+numerical_vars = ['age', 'price', 'duration_in_mins', 'price_per_min' , 'images_count','exp_ads_from_simimages_count','similar_images_count']
+phone_level_vars = ['n_ads', 'n_distinct_locations', 'location_tree_length', 'n_outcall', 'n_incall', 'n_incall_and_outcall', 'n_cooccurring_phones']
+#continuous_means = df.groupby('phone')[continuous_vars].mean()
+#continuous_means = continuous_means.rename(columns = {k+'_mean':k for k in continuous_means.columns})
+#continuous_stds = df.groupby('phone')[continuous_vars].std()
+#continuous_stds = continuous_stds.rename(columns = {k+'_std':k for k in continuous_stds.columns})
 #continuous_sums = df.groupby('phone')[continuous_vars].sum()
 #continuous_sums = continuous_sums.rename(columns = {k+'_sum':k for k in continuous_sums.columns})
-#continuous = pd.concat([continuous_means, continuous_sums], axis=1)
-continuous = continuous_means
+#continuous = pd.concat([continuous_means, continuous_stds], axis=1)
+numerical = df.groupby('phone')[numerical_vars].describe().unstack() # add distributions of these continuous variables
+phone_level_vars = df.groupby('phone')[phone_level_vars].max()
+#continuous = continuous_means
 flag_dummies = pd.get_dummies(df['flag'])
 flag_dummies = pd.concat([df['phone'], flag_dummies], axis=1)
-discrete = flag_dummies.groupby('phone').aggregate([np.mean, np.sum])
-phone_level = pd.concat([continuous, discrete], axis=1)
+#discrete = flag_dummies.groupby('phone').aggregate([np.mean, np.sum])
+discrete = flag_dummies.groupby('phone').mean()
+phone_level = pd.concat([numerical, discrete, phone_level_vars], axis=1)
+phone_level['has_images'] = df.groupby('phone')['has_images'].max()
 phone_level_label = df.groupby('phone')['class'].max()
 phone_level.index = range(len(phone_level))
 phone_level = phone_level.reindex()
@@ -184,12 +193,22 @@ def all_scoring_metrics(clf, X, y, stratified_kfold):
         clf.fit(X.loc[train], y.loc[train])
         y_pred = clf.predict(X.loc[test])
         y_test=y.loc[test]
-
-    output_features = score_metrics(y_test, y_pred)
-    output_features.update({i[0]:i[1] for i in zip(X.columns, clf.feature_importances_)})
-    output_features['roc_auc'] = roc_auc_score(y_test, clf.predict_proba(X.loc[test])[:,1])
-    out.append(output_features)
+        output_features = score_metrics(y_test, y_pred)
+        output_features.update({i[0]:i[1] for i in zip(X.columns, clf.feature_importances_)})
+        output_features['roc_auc'] = roc_auc_score(y_test, clf.predict_proba(X.loc[test])[:,1])
+        out.append(output_features)
     return pd.DataFrame(out)
+
+#def phone_number_stratification(phone_numbers, X, y, num_folds, X_phone_column='phone'):
+    #'''
+    #phone_numbers is a list of the phone numbers that will be drawn from. X will have a column with these phone numbers.
+    #'''
+    #out = []
+    #me=StratifiedKFold(phone_numbers, 3)
+    #for i, (train, test) in enumerate(me):
+        #ipdb.set_trace()
+
+#me = phone_number_stratification(df['phone'], df, df['class'], 2)
 
 eval_columns = ['f1','accuracy','true_negative_rate','true_positive_rate','roc_auc']
 price_cols = ['duration_in_mins','price','price_per_min']
@@ -197,25 +216,29 @@ price_cols = ['duration_in_mins','price','price_per_min']
 # work at phone level
 print("_____")
 print("Work at phone level...")
+num_folds = 10
 phone_clf = RandomForestClassifier(oob_score=True, random_state=2, n_estimators=100, n_jobs=-1, class_weight="balanced")
-metrics = all_scoring_metrics(phone_clf, phone_level, phone_level_label, StratifiedKFold(phone_level_label, 10))
-print("Results (averaged from 10 fold cross validation and computed out of sample)")
-print(metrics[[i for i in metrics.columns if i in eval_columns]])
-#importances = metrics[[i for i in metrics.columns if i not in eval_columns]]
-#print('Price importances: %s' % importances[[i for i in importances.columns if i in price_cols]].sum(axis=1).iloc[0])
+metrics = all_scoring_metrics(phone_clf, phone_level, phone_level_label, StratifiedKFold(phone_level_label, num_folds))
+print("Results (averaged from %s fold cross validation and computed out of sample)" % num_folds)
+means = metrics.mean()[[i for i in metrics.columns if i in eval_columns]]
+stds = metrics.std()[[i for i in metrics.columns if i in eval_columns]]
+print(pd.concat([means,stds], axis=1).rename(columns={0:'mean',1:'std'}))
+#importances = metrics.mean()[[i for i in metrics.columns if i not in eval_columns]]
+#print('Price importances: %s' % importances[[i for i in importances.columns if 'price' in i or 'dur' in i]].sum(axis=1).iloc[0])
+#print('Age importances: %s' % importances[[i for i in importances.columns if 'age' in i]].sum(axis=1).iloc[0])
 #print('Age importances: %s' % importances['age'].iloc[0])
 #p.close()
 #p.join()
 
 
-# work at ad level
-print("_____")
-print("Work at ad level...")
-clf = RandomForestClassifier(oob_score=True, random_state=2, n_estimators=100, n_jobs=-1, class_weight="balanced")
-metrics = all_scoring_metrics(clf, df_X, df['class'], StratifiedKFold(df['class'], 2))
-print("Results (averaged from 10 fold cross validation and computed out of sample)")
-print(metrics[[i for i in metrics.columns if i in eval_columns]])
-importances = metrics[[i for i in metrics.columns if i not in eval_columns]]
-print('Price importances: %s' % importances[[i for i in importances.columns if i in price_cols]].sum(axis=1).iloc[0])
-print('Age importances: %s' % importances['age'].iloc[0])
+## work at ad level
+#print("_____")
+#print("Work at ad level...")
+#clf = RandomForestClassifier(oob_score=True, random_state=2, n_estimators=100, n_jobs=-1, class_weight="balanced")
+#metrics = all_scoring_metrics(clf, df_X, df['class'], StratifiedKFold(df['class'], 2))
+#print("Results (averaged from 10 fold cross validation and computed out of sample)")
+#print(metrics[[i for i in metrics.columns if i in eval_columns]])
+#importances = metrics[[i for i in metrics.columns if i not in eval_columns]]
+#print('Price importances: %s' % importances[[i for i in importances.columns if i in price_cols]].sum(axis=1).iloc[0])
+#print('Age importances: %s' % importances['age'].iloc[0])
 
